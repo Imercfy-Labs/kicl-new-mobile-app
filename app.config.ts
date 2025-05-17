651bbcb3fbc70c9161c2d5fea6cd2b364e37443e
Imercfy-Labs <tools@imercfy.com>
2025-05-17 13:37:30 +0530

    Updated app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -22,7 +22,7 @@ export default {
   },
   android: {
     adaptiveIcon: {
-      // foregroundImage: './assets/images/adaptive-icon.png',
+      foregroundImage: './assets/images/favicon.png',
       backgroundColor: '#FFFFFF'
     },
     package: 'com.kicl.mobile'
